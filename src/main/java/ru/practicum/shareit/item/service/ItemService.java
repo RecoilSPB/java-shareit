51e89151b2bb22c6f5c48b0446f3f5b47dfcf065
Никита Попов <recoilvrn@gmail.com>
2024-11-04 23:25:00 +0300

    Убрана аннотация @NotNull, так как проверка осуществляется на стороне контроллера

--- src/main/java/ru/practicum/shareit/item/service/ItemService.java
+++ src/main/java/ru/practicum/shareit/item/service/ItemService.java
@@ -1,16 +1,15 @@
 package ru.practicum.shareit.item.service;
 
-import jakarta.validation.constraints.NotNull;
 import ru.practicum.shareit.item.dto.ItemDto;
 
 import java.util.List;
 
 public interface ItemService {
-    List<ItemDto> getItemsByUser(@NotNull Long userId);
+    List<ItemDto> getItemsByUser(Long userId);
 
     List<ItemDto> getItemByText(String text);
 
-    ItemDto getItemById(Long itemId, @NotNull Long userId);
+    ItemDto getItemById(Long itemId, Long userId);
 
     ItemDto createItem(Long userId, ItemDto itemDto);
 
